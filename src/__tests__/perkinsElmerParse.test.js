ac765434a87cc081aa3892e3cf56654bb1573a9a
Giustino Sulpizio <giustino.sulpizio@epfl.ch>
2021-01-06 03:43:30 +0100

    chore: add new examples to perkinsElmerParse test case

--- src/__tests__/perkinsElmerParse.test.js
+++ src/__tests__/perkinsElmerParse.test.js
@@ -7,7 +7,16 @@ describe('test perkinsElmerParse', () => {
   it('should return object with the info from file rawTGA.xlsx', () => {
     let file = readFileSync(join(__dirname, '/example/rawTGA.xlsx'));
     let result = perkinsElmerParse(file);
+    //console.log(result);
+  });
+  it('should return object with the info from file rawTGA2.xlsx', () => {
+    let file = readFileSync(join(__dirname, '/example/rawTGA2.xlsx'));
+    let result = perkinsElmerParse(file);
+    //console.log(result);
+  });
+  it('should return object with the info from file rawTGA9.xlsx', () => {
+    let file = readFileSync(join(__dirname, '/example/rawTGA9.xlsx'));
+    let result = perkinsElmerParse(file);
     console.log(result);
-    //expect(getMatrixFromXLSX(file)).toStrictEqual([['a', 'b'],['c', 'd'],]);
   });
 });
